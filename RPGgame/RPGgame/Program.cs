06fcf5a80a95fc80c846dc08b23d0e0d5d398b39
Vitaliy Ivanov <ivanoff4123@gmail.com>
2023-06-03 13:42:09 +0700

    added SFML

--- RPGgame/RPGgame/Program.cs
+++ RPGgame/RPGgame/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+
+using SFML;
+using SFML.Graphics;
+using SFML.Window;
 
 namespace RPGgame
 {
@@ -10,6 +13,16 @@ namespace RPGgame
     {
         static void Main(string[] args)
         {
+            RenderWindow win = new RenderWindow(new VideoMode(600, 600), "SFML");
+
+            while(win.IsOpen)
+            {
+                win.DispatchEvents();
+
+                win.Clear();
+
+                win.Display();
+            }
         }
     }
 }
