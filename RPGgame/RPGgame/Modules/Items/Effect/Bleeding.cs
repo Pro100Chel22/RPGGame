fbc79f0468b190a4fbc07e64140c7126c3dadf11
Viktoria LegenPray <Pvi22012004@gmail.com>
2023-06-16 19:53:50 +0700

    Effect без freeze and stun

---
+++ RPGgame/RPGgame/Modules/Items/Effect/Bleeding.cs
@@ -0,0 +1,23 @@
+﻿using RPGgame.Modules.Entitys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGgame.Modules.Items.Effect
+{
+    internal class Bleeding:TemporaryEffect
+    {
+        public override void Employ(IAlive Alive)
+        {
+            throw new Exception("Employ недоступен, так как функция не реализована");
+
+        }
+        public override void Delete(IAlive Alive)
+        {
+            throw new Exception("Delete недоступен, так как функция не реализована");
+
+        }
+    }
+}
